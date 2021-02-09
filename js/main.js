8ea32b59e8cd8e08161ec01a067529d9118c0ca5
smirnovanv <smirnovanv89@gmail.com>
2021-02-09 16:06:47 +0300

    Правки по критериям

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
-const getRandomInRange = function (min, max) {
-  if (min >= 0 && max > 0 && min < max) {return Math.floor(Math.random() * (max - min + 1)) + min
-  } else {return 'Ошибка'}
-}
-
-getRandomInRange(0, 100);
-
-//Источник кода: https://myrusakov.ru/js-random-numbers.html
-
-const getRandomFloatNumber = function (minNumber, maxNumber, numbersAfterComma) {
-  if (minNumber >= 0 && maxNumber > 0 && minNumber < maxNumber) {
-    return Number(((Math.random() * (maxNumber - minNumber)) + minNumber).toFixed(numbersAfterComma))
-  } else {return 'Ошибка'}
-}
-
-getRandomFloatNumber(5, 5.005, 3);
-
-//Источник кода: мой код
-
-//Задание №3 Создание 10 предложений жилья
+'use strict';
 
 const PROPERTY_TYPES = ['palace', 'flat', 'house', 'bungalow'];
 const OFFERED_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -38,7 +19,7 @@ const OFFER_PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
-const OFFER_DESCRIPTION = [
+const OFFER_DESCRIPTIONS = [
   'Яркий и броский дизайн апартаментов подойдет активным людям. В числе стандартных удобств — телевизор с плоским экраном и спутниковыми каналами, а также мини-сейф.',
   'Дизайн апартаментов выполнен в пастельных тонах. В стоимость входит еженедельная уборка и смена постельного белья, а также приветственный продуктовый набор по прибытии.',
   'Стильно и просто. Это любимая часть города среди наших гостей согласно независимым отзывам.',
@@ -53,7 +34,33 @@ const OFFER_DESCRIPTION = [
 //описания взяты с booking.com
 const SIMILAR_OFFERS_COUNT = 10;
 
-let createNewArray = function (originalArray) {
+const getRandomInRange = function (min, max) {
+  if (min >= 0 && max > 0 && min < max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  else {
+    return null;
+  }
+}
+
+getRandomInRange(0, 100);
+
+//Источник кода: https://myrusakov.ru/js-random-numbers.html
+
+const getRandomFloatNumber = function (minNumber, maxNumber, numbersAfterComma) {
+  if (minNumber >= 0 && maxNumber > 0 && minNumber < maxNumber) {
+    return Number(((Math.random() * (maxNumber - minNumber)) + minNumber).toFixed(numbersAfterComma));
+  }
+  else {
+    return null;
+  }
+}
+
+getRandomFloatNumber(5, 5.005, 3);
+
+//Источник кода: мой код
+
+const createRandomArray = function (originalArray) {
   let elementsNumber = getRandomInRange(1, originalArray.length);
   let shuffledElements = originalArray.sort(() => Math.random() - 0.5);
   let newArray = [];
@@ -68,29 +75,29 @@ const createApartment = function () {
     avatar: 'img/avatars/user0' + getRandomInRange(1, 8) + '.png',
   };
 
+  let location = {
+    x: getRandomFloatNumber(35.65000, 35.70000, 5),
+    y: getRandomFloatNumber(139.70000, 139.80000, 5),
+  };
+
   let offer = {
     title: OFFER_TITLES[getRandomInRange(0, OFFER_TITLES.length -1)],
-    address: '{{location.x}}, {{location.y}}',
+    address: `${location.x}, ${location.y}`,
     price: getRandomInRange(1, 1000000),
     type: PROPERTY_TYPES[getRandomInRange(0, PROPERTY_TYPES.length -1)],
     rooms: getRandomInRange(1, 100),
     guests: getRandomInRange(0, 3),
     checkin: getRandomInRange(12, 14) + ':00',
     checkout: getRandomInRange(12, 14) + ':00',
-    features: createNewArray(OFFERED_FEATURES),
-    description: OFFER_DESCRIPTION[getRandomInRange(0, OFFER_DESCRIPTION.length -1)],
-    photos: createNewArray(OFFER_PHOTOS),
-  };
-
-  let location = {
-    x: getRandomFloatNumber(35.65000, 35.70000, 5),
-    y: getRandomFloatNumber(139.70000, 139.80000, 5),
+    features: createRandomArray(OFFERED_FEATURES),
+    description: OFFER_DESCRIPTIONS[getRandomInRange(0, OFFER_DESCRIPTIONS.length -1)],
+    photos: createRandomArray(OFFER_PHOTOS),
   };
 
   return {author, offer, location}
 }
 
-let createSimilarOffers = function () {
+const createSimilarOffers = function () {
   let similarOffers = new Array(SIMILAR_OFFERS_COUNT).fill(null).map(() => createApartment());
   return similarOffers;
 }
